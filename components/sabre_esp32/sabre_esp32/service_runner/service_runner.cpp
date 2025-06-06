408adc29c206f48c50f9a230a78a9ca973039e86
Daryl Stark <daryl@dstark.nl>
2025-06-06 20:32:10 +0000

    feat: Add ServiceRunner class and implementation.
    This can and should be used for services like a MQTT client and a
    webservice.

---
+++ components/sabre_esp32/sabre_esp32/service_runner/service_runner.cpp
@@ -0,0 +1,33 @@
+#include "service_runner.h"
+
+namespace sabre::esp32
+{
+    void ServiceRunner::_runner(void *obj)
+    {
+        auto *_obj = static_cast<ServiceRunner *>(obj);
+        _obj->_service();
+        _obj->stop();
+    }
+
+    void ServiceRunner::start()
+    {
+        xTaskCreate(&ServiceRunner::_runner, "ServiceTask", 2048, this, 5,
+                    &_freertos_handle);
+        _is_running = true;
+    }
+
+    void ServiceRunner::stop()
+    {
+        if (_freertos_handle)
+        {
+            _freertos_handle = nullptr;
+            _is_running = false;
+            vTaskDelete(_freertos_handle);
+        }
+    }
+
+    bool ServiceRunner::is_running() const
+    {
+        return _is_running;
+    }
+} // namespace sabre::esp32
--- src/main.cpp
+++ src/main.cpp
@@ -1,150 +1,44 @@
 #include <freertos/FreeRTOS.h>
-#include <freertos/queue.h>
-#include <freertos/task.h>
-
-#include <cstring>
-#include <iomanip>
-#include <sabre/uart/uart_output_stream_buffer.h>
-#include <sabre_esp32/esp32_factory.h>
-#include <sabre_esp32/gpio/input_gpio.h>
+#include <functional>
+#include <iostream>
+#include <memory>
 #include <sabre_esp32/gpio/output_gpio.h>
-#include <sabre_esp32/uart/uart.h>
-#include <string>
+#include <sabre_esp32/service_runner/service_runner.h>
 
-#include <driver/gpio.h>
-
-#include <sabre_esp32/exceptions/exceptions.h>
-
-#include <sabre/logging/logging.h>
-
-#include <sabre/logging/log_handlers.h>
-
-#include <sabre/generic/ipv4_address.h>
-#include <sabre_esp32/wifi/wifi_station.h>
-
-#include <sabre_esp32/wifi/wifi_soft_ap.h>
-
-#include <sabre_esp32/utility/timed_waiter.h>
-
-QueueHandle_t interruptQueue;
-
-void very_special_isr_handler(int x)
-{
-    xQueueSendFromISR(interruptQueue, &x, NULL);
-}
-
-void led_control_task(void *params)
-{
-    int pinNumber;
-    while (true)
-    {
-        if (xQueueReceive(interruptQueue, &pinNumber, portMAX_DELAY))
-        {
-            gpio_set_level(
-                GPIO_NUM_2,
-                gpio_get_level(static_cast<gpio_num_t>(pinNumber)) == 1 ? 0
-                                                                        : 1);
-        }
-    }
-}
-
-class Application
+class MyService : public sabre::esp32::ServiceRunner
 {
 private:
-    std::shared_ptr<sabre::Factory> _os_factory;
-
-    // Logging
-    std::shared_ptr<sabre::Logger> _logger;
-
-    // For output
-    std::shared_ptr<sabre::UARTStreamBuf> _uart_stream_buf;
-    std::shared_ptr<sabre::UARTStreamBuf> _uart_stream_buf_2;
-    std::ostream _u0o;
-    std::ostream _u2o;
-
-    std::shared_ptr<sabre::OutputGPIO> _led_gpio;
-    std::shared_ptr<sabre::InputGPIO> _button;
-
-    std::shared_ptr<sabre::WifiStation> _wifi_station;
-
-    std::shared_ptr<sabre::WifiSoftAP> _wifi_soft_ap;
+    bool _state = false;
+    sabre::esp32::OutputGPIO _gpio;
+    uint16_t _interval;
 
 protected:
-    std::shared_ptr<sabre::OutputGPIO> _get_led_gpio() const
+    void _service() override
     {
-        std::shared_ptr<sabre::OutputGPIO> gpio =
-            _os_factory->create_output_gpio(2);
-        return gpio;
-    }
-
-    std::shared_ptr<sabre::InputGPIO> _get_button_gpio() const
-    {
-        std::shared_ptr<sabre::InputGPIO> gpio =
-            _os_factory->create_input_gpio(26);
-        gpio->enable_pullup();
-        gpio->set_inverse_level();
-        gpio->add_interrupt_handler(very_special_isr_handler,
-                                    sabre::ISRTrigger::BOTH);
-        return gpio;
+        while (true)
+        {
+            _state = !_state;
+            _gpio.set_level(_state);
+            vTaskDelay(pdMS_TO_TICKS(_interval));
+        }
     }
 
 public:
-    Application(std::shared_ptr<sabre::Factory> factory)
-        : _os_factory(factory), _u0o(nullptr), _u2o(nullptr)
+    MyService(uint16_t interval) : _gpio(2), _interval(interval) {}
+    void set_interval(uint16_t interval)
     {
-        // Wait a bit to make sure the bootloader logging is done
-        vTaskDelay(100 / portTICK_PERIOD_MS);
-
-        // Configure output stream
-        _uart_stream_buf =
-            _os_factory->create_uart_output_stream_buffer(0, 115200, 1, 3, 8);
-        _u0o.rdbuf(_uart_stream_buf.get());
-
-        // Configure logging
-        auto ostream_log_handler =
-            std::make_shared<sabre::OStreamLogHandler>(_u0o);
-        _logger = std::make_shared<sabre::Logger>("APP");
-        sabre::Logging::set_level(sabre::LoggingLevel::DEBUG);
-        sabre::Logging::add_handler(ostream_log_handler);
-
-        _logger->info("Starting application...");
-
-        // Configure WiFi
-        _u0o << "Starting AP mode" << std::endl;
-        _wifi_soft_ap = _os_factory->create_wifi_soft_ap();
-        _wifi_soft_ap->init();
-        _wifi_soft_ap->start("ESP32-test", "testtest");
-
-        vTaskDelay(5000 / portTICK_PERIOD_MS);
-
-        _u0o << "Starting station mode" << std::endl;
-        _wifi_station = _os_factory->create_wifi_station();
-        _wifi_station->init();
-        _wifi_station->connect("SSID", "PASSWORD");
-
-        vTaskDelay(5000 / portTICK_PERIOD_MS);
-        _u0o << "Stopping station mode" << std::endl;
-        _wifi_station->stop();
-
-        vTaskDelay(5000 / portTICK_PERIOD_MS);
-        _u0o << "Stopping AP mode" << std::endl;
-        _wifi_soft_ap->stop();
-
-        // Configure GPIOs
-        _led_gpio = _get_led_gpio();
-        _button = _get_button_gpio();
-
-        // Create FreeRTOS tasks
-        xTaskCreate(led_control_task, "LED_Control_Task", 2048, NULL, 1, NULL);
-
-        _logger->info("Bootup done.");
+        _interval = interval;
     }
+};
 
-    void run_loop()
+class Application
+{
+    std::shared_ptr<MyService> _service;
+
+public:
+    Application() : _service(std::make_shared<MyService>(500))
     {
-        _logger->info("Starting loop");
-        while (true)
-            vTaskDelay(1000 / portTICK_PERIOD_MS);
+        _service->start();
     }
 };
 
@@ -152,10 +46,10 @@ extern "C"
 {
     void app_main(void)
     {
-        interruptQueue = xQueueCreate(10, sizeof(int));
-        gpio_install_isr_service(0); // TODO: Move to Application class
-        Application app(std::make_shared<sabre::esp32::ESP32Factory>());
-        app.run_loop();
+        Application app;
+
+        while (true)
+            vTaskDelay(pdMS_TO_TICKS(1000));
 
         return;
     }
