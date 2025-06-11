e72a815dd75a6c117967c631781a05d1e36ee75e
Daryl Stark <daryl@dstark.nl>
2025-06-11 20:14:40 +0000

    refactor: Simplify return types.

--- components/sabre_esp32/sabre_esp32/esp32_factory.cpp
+++ components/sabre_esp32/sabre_esp32/esp32_factory.cpp
@@ -7,14 +7,14 @@ namespace sabre::esp32
     ESP32Factory::create_uart_object(uint32_t uart_number, int32_t baud_rate,
                                      int32_t tx_pin, int32_t rx_pin) const
     {
-        return std::make_shared<sabre::esp32::UART>(
-            static_cast<uart_port_t>(uart_number), baud_rate, tx_pin, rx_pin);
+        return std::make_shared<UART>(static_cast<uart_port_t>(uart_number),
+                                      baud_rate, tx_pin, rx_pin);
     }
 
     std::shared_ptr<sabre::InputGPIO>
     ESP32Factory::create_input_gpio(int32_t pin) const
     {
-        return std::make_shared<sabre::esp32::InputGPIO>(pin);
+        return std::make_shared<InputGPIO>(pin);
     }
 
     std::shared_ptr<sabre::OutputGPIO>
