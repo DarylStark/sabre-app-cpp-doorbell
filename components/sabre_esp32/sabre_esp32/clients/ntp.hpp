9cab337b44fa3827867e9a894b682bb8ffab7e90
Daryl Stark <daryl@dstark.nl>
2025-06-24 18:52:24 +0000

    docs: Add documentation to `NTPClient`.

--- components/sabre_esp32/sabre_esp32/clients/ntp.hpp
+++ components/sabre_esp32/sabre_esp32/clients/ntp.hpp
@@ -12,9 +12,33 @@ namespace sabre::esp32
         std::string _server;
 
     public:
+        /**
+         * @brief Constructs the NTPClient object.
+         *
+         * @param server The NTP server to use.
+         */
         NTPClient(const std::string &server);
+
+        /**
+         * @brief Starts the NTP client.
+         *
+         * This will initialize the NTP client and start synchronizing time
+         * with the specified NTP server.
+         */
         void start();
+
+        /**
+         * @brief Stops the NTP client.
+         *
+         * This will stop the NTP client and cease time synchronization.
+         */
         void stop();
+
+        /**
+         * @brief Checks if the NTP client is synchronized.
+         *
+         * @returns true if the NTP client is synchronized, false otherwise.
+         */
         bool is_synchronized() const;
     };
 } // namespace sabre::esp32
